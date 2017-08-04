cad69d79d09a8bb8d8c44fd7e54b96a15b843c12
Waqas84 <walsubayee@yahoo.com>
2017-08-04 11:29:43 -0700

    signup & login pages added

--- config/passport.js
+++ config/passport.js
@@ -30,6 +30,7 @@ passport.use(new LocalStrategy(
             }
             // If none of the above, return the user
             return done(null, dbUser);
+            console.log("dbUser: ", dbUser)
         });
     }
 ));
