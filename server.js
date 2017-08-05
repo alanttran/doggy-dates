c4b14018581843689bee787bc437b5dbe1f83f00
Margaret Choi <margaret.j.li@gmail.com>
2017-08-04 23:59:36 -0700

    Require HTML route

--- server.js
+++ server.js
@@ -27,6 +27,8 @@ let exphbs = require("express-handlebars");
 app.engine("handlebars", exphbs({ defaultLayout: "main" }));
 app.set("view engine", "handlebars")
 
+// Requiring routes
+require("./routes/html-routes.js")(app);
 require("./routes/api-routes.js")(app);
 
 // Syncing our database and logging a message to the user upon success
