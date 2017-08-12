22f3db5db101362a0c03aca7f0c05172ee92f00d
alanttran <mr.alan.tran@gmail.com>
2017-08-12 01:41:27 -0700

    created seeds.sql, tweaked ui some more

---
+++ db/seeds.sql
@@ -0,0 +1,6 @@
+insert into dogs (dog_name, image, zip_code,dob,energy_level, sex, size)
+values 
+("Biscuit", "http://www.randomdoggiegenerator.com/", 92123, "2017-08-11 07:00:00", "high", "male", "small"),
+("Bella", "https://www.what-dog.net/Images/faces2/scroll0015.jpg", 93292, "2017-08-11 07:00:00", "low", "female", "large"),
+("Max", "https://lh4.ggpht.com/pUuYNPWDPk0aLsCKUYS_tYxRvxXkbGpxbC45IIxn0pQSbvJ_8uadrR-ro7fUtFGJw14=h900", 99875, "2017-08-11 07:00:00", "medium", "male", "xlarge"),
+("Molly", "https://www.rspcawa.asn.au/perch/resources/puppy-w800.jpg", 92131, "2017-08-11 07:00:00", "extra", "female", "medium");
